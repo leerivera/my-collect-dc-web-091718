63de044932b55d6aeb09e90c51dad63eecc640a9
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-09-03 17:29:51 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -7,18 +7,16 @@
  def my_collect(array)
    new_arr = [] 
   
-   i=0 
+   i = 0 
    while i < array.length 
    new_arr.push yield(array[i])
-   i += 1 
- end 
+ end
      new_arr
  end 
 
 
- my_collect(student) do |name| 
-   name.split(" ").first 
+ my_collect(student) do 
+    
 end 
- my_collect(collection) do |lang|
- lang.upcase 
+ my_collect(collection) do 
 end
